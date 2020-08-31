149d4793e79bd2bb5c37e0ac83ac071a5bafdaf7
MrR3set <bairon.pazrobles@gmail.com>
2020-08-30 18:23:05 -0700

    Dummy data

---
+++ src/components/Projects/projectsData.js
@@ -0,0 +1,20 @@
+export const projectData = [
+  {
+    "id": 249778626,
+    "name": "Name Here",
+    "html_url": "https://github.com/MrR3set/Auth-Friends",
+    "archived": false,
+    "featured":true,
+    "description": null,
+    "deployed_url": "https://baironpaz.com",
+    "clone_url": "https://github.com/MrR3set/Auth-Friends.git",
+    "created_at": "2020-03-24T17:51:16Z",
+    "updated_at": "2020-03-24T17:51:18Z",
+    "media":null,
+    "tools":[
+      "JavaScript",
+      "React.JS",
+      "Node.JS"
+    ]
+  },
+]
