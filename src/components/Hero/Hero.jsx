9cbe83c9d5a79389b13a817f3d53bded6e6a8021
MrR3set <bairon.pazrobles@gmail.com>
2020-08-30 18:22:52 -0700

    Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function Hero() {
-  return (
-    <div className="App">
-      
+    return (
+    <div className="hero-wrapper">
+        <div className="hero">
+            Bringing ideas
+            <br/>to life
+        </div>
     </div>
-  );
+    );
 }
 
 export default Hero;
