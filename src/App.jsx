2757504e9289278f43948db50f8cacffedfd3634
MrR3set <bairon.pazrobles@gmail.com>
2020-09-02 21:10:30 -0700

    Added app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Projects from "./components/Projects/Projects"
+import Hero from "./components/Hero/Hero"
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero></Hero>
+      <Projects></Projects>
     </div>
   );
 }
