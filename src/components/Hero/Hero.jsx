7053d40109d40c602f657c21fd977743d24d734e
MrR3set <bairon.pazrobles@gmail.com>
2020-09-03 14:55:31 -0700

    Finished hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import ArrowIcon from "./Arrow"
+import "./hero.scss"
+import Bounce from 'react-reveal/Bounce';
 
 function Hero() {
     return (
     <div className="hero-wrapper">
+        <div className="background">
+            <img src={require("./background.jpg")} alt="Hero background"/>
+        </div>
         <div className="hero">
             Bringing ideas
-            <br/>to life
+            <br/>to <span>life</span>
         </div>
+        <ArrowIcon/>
     </div>
     );
 }
