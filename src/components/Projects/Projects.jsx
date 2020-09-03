f168c5f92b0b81ee2b9c7302087d1942cad5813f
MrR3set <bairon.pazrobles@gmail.com>
2020-09-02 21:10:37 -0700

    Removed unused code

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,14 +2,8 @@ import React, { useState, useEffect }  from 'react';
 import Project from "./Project"
 import {projectData} from "./projectsData"
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); // integer state
-    return () => setValue(value => ++value); // update the state to force render
-}
 
 function Projects() {
-
-    const forceUpdate = useForceUpdate();
 
     return (
     <div className="projects-wrapper">
