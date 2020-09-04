b3658e6e8d79815b2e35f5f77d6fa39fdea3622f
MrR3set <bairon.pazrobles@gmail.com>
2020-09-03 17:31:34 -0700

    Set up project data

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,19 +1,36 @@
 export const projectData = [
   {
-    "id": 249778626,
-    "name": "Name Here",
-    "html_url": "https://github.com/MrR3set/Auth-Friends",
+    "id": 1,
+    "title": "Data Driven transit",
+    "organization":"Lambda school",
     "archived": false,
     "featured":true,
-    "description": null,
-    "deployed_url": "https://baironpaz.com",
-    "clone_url": "https://github.com/MrR3set/Auth-Friends.git",
+    "description": "Create a publicly available database of historical locations of bus and rail cars. Provide near real time analysis of travel times from major travel corridors. Give citizens, oversight committee members, and SFMTA staff accurate and timely historical data, along with statistics and analysis, to make informed decisions to measure system wide improvements.",
+    "deployed_url": "https://sfmta-data-analysis-fe-inky.vercel.app/",
+    "github_url": "https://github.com/Lambda-School-Labs/sfmta-data-analysis-fe",
     "date":"Jun 2020",
-    "media":null,
+    "media":"./assets/datadriven.png",
     "tools":[
-      "JavaScript",
       "React.JS",
-      "Node.JS"
+      "Express.JS",
+      "PostgreSQL"
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Deeptunes",
+    "organization":"Lambda school",
+    "archived": false,
+    "featured":true,
+    "description": "Deeptunes is a React application that allows users to find song recommendations utilizing a deep learning algorithm developed by Data Scientits students at lambda school.",
+    "deployed_url": "https://deeptunes.now.sh/",
+    "github_url": "https://github.com/Lambda-Spotify-Song-Suggester-3",
+    "date":"Apr 2020",
+    "media":"./assets/deeptunes.png",
+    "tools":[
+      "React.JS",
+      "Node.JS",
+      "PostgreSQL"
     ]
   },
 ]
