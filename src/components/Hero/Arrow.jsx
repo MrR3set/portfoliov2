ee6b7519e38e18b7bd77219fceeed8267d6af2be
MrR3set <bairon.pazrobles@gmail.com>
2020-09-03 17:42:37 -0700

    Onclick scroll to

--- src/components/Hero/Arrow.jsx
+++ src/components/Hero/Arrow.jsx
@@ -2,7 +2,13 @@ import React from "react"
 
 export default function ArrowIcon(){
     return(
-<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg onClick={(e)=>{
+    e.preventDefault();
+    // window.scrollBy(0,500)  
+    window.scrollTo({top:950,left:0,behavior:"smooth"})
+}}
+
+version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             viewBox="0 0 490.688 490.688" xmlSpace="preserve">
         <path d="M472.328,120.529L245.213,347.665L18.098,120.529c-4.237-4.093-10.99-3.975-15.083,0.262
             c-3.992,4.134-3.992,10.687,0,14.82l234.667,234.667c4.165,4.164,10.917,4.164,15.083,0l234.667-234.667
