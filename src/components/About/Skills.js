3feacd4e82b8172dbebcb30d6e8879d0f30fbd69
MrR3set <bairon.pazrobles@gmail.com>
2020-09-04 21:09:11 -0700

    Added skill data

---
+++ src/components/About/Skills.js
@@ -0,0 +1,62 @@
+export const Skills = [
+    {
+        "id":"1",
+        "name":"HTML",
+        "time":"4 years"
+    },
+    {
+        "id":"2",
+        "name":"CSS",
+        "time":"3 years"
+    },
+    {
+        "id":"3",
+        "name":"C",
+        "time":"3 years"
+    },
+    {
+        "id":"4",
+        "name":"C#",
+        "time":"3 years"
+    },
+    {
+        "id":"5",
+        "name":"C++",
+        "time":"3 years"
+    },
+    {
+        "id":"6",
+        "name":"Java",
+        "time":"3 years"
+    },
+    {
+        "id":"7",
+        "name":"Python",
+        "time":"3 Months"
+    },
+    {
+        "id":"8",
+        "name":"Git",
+        "time":"1 Year"
+    },
+    {
+        "id":"9",
+        "name":"React",
+        "time":"9 Months"
+    },
+    {
+        "id":"10",
+        "name":"PostgreSQL",
+        "time":"5 Months"
+    },
+    {
+        "id":"11",
+        "name":"Node.JS",
+        "time":"2 years"
+    },
+    {
+        "id":"12",
+        "name":"Typescript",
+        "time":"2 years"
+    },
+]
